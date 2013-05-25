baba6dfd7889b5fceef7e1b3c8bc1f150bf86298
John Wiseman <jwiseman@morseproject.com>
2013-05-25 16:04:55 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express')
+  , app = express()
+  , fs = require('fs')
+  , path = require('path')
+  , server = require("http").createServer(app)
+  , io = require('socket.io').listen(server)
+  , arDrone = require('ar-drone')
+  ;
+
+// Override the drone ip using an environment variable,
+// using the same convention as node-ar-drone
+var drone_ip = process.env.DEFAULT_DRONE_IP || '192.168.1.1';
+
+// Keep track of plugins js and css to load them in the view
+var scripts = []
+  , styles = []
+  ;
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs', { pretty: true });
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler());
+    app.locals.pretty = true;
+});
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'AR.Drone THREE'
+        ,scripts: scripts
+        ,styles: styles
+    });
+});
+
+/*
+ * Important:
+ *
+ * pass in the server object to listen, not the express app
+ * call 'listen' on the server, not the express app
+ */
+
+// Connect and configure the drone
+var client = new arDrone.createClient();
+client.config('general:navdata_demo', 'TRUE');
+client.config('video:video_channel', '0');
+
+// Add a handler on navdata updates
+var latestNavData;
+client.on('navdata', function (d) {
+    latestNavData = d;
+});
+
+// Process new websocket connection
+io.set('log level', 1);
+io.sockets.on('connection', function (socket) {
+  socket.emit('event', { message: 'Welcome to cockpit :-)' });
+});
+
+// Schedule a time to push navdata updates
+var pushNavData = function() {
+    io.sockets.emit('navdata', latestNavData);
+};
+var navTimer = setInterval(pushNavData, 100);
+
+// Start the web server
+server.listen(app.get('port'), function() {
+  console.log('AR.Drone THREE is listening on port ' + app.get('port'));
+});
