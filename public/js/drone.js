0e36c4c8f852e1f9f475c157109212136dd7e032
John Wiseman <jwiseman@morseproject.com>
2013-05-26 13:32:17 -0700

    Added live update.

--- public/js/drone.js
+++ public/js/drone.js
@@ -1,8 +1,5 @@
-var Drone = function(navdata) {
+var Drone = function() {
   THREE.Object3D.call(this);
-  this.navdata = navdata;1
-  this.stepNum = 0;
-
   var material = new THREE.MeshBasicMaterial({
     color: 0xff0000,
   });
@@ -13,15 +10,11 @@ var Drone = function(navdata) {
     //result.scene.rotation = new THREE.Vector3(-Math.PI / 2, 0, -Math.PI / 2);
     self.add(result.scene);
   });
-
 }
 Drone.prototype = Object.create(THREE.Object3D.prototype);
 
-Drone.prototype.step = function() {
-  var state = this.navdata[Math.floor(this.stepNum / 3)];
-  this.stepNum = (this.stepNum + 1) % (this.navdata.length * 3);
-  var r = state.demo.drone.camera.rotation;
-  var t = state.demo.drone.camera.translation;
+Drone.prototype.updateState = function(navdata) {
+  var r = navdata.demo.drone.camera.rotation;
   var m = new THREE.Matrix4(r.m11, r.m12, r.m13, 0,
                             r.m21, r.m22, r.m23, 0,
                             r.m31, r.m32, r.m33, 0,
@@ -30,10 +23,3 @@ Drone.prototype.step = function() {
   this.matrixAutoUpdate = false;
   this.updateMatrixWorld(true);
 };
-
-Drone.prototype.moveTo = function(x, y, z) {
-  this.position.x = x;
-  this.position.y = y;
-  this.position.z = z;
-  this.lowVel = 0;
-}
