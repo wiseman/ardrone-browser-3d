c029e52d02d3fd6398dfa623fdb1a21db01ee436
John Wiseman <jwiseman@morseproject.com>
2013-05-27 13:51:27 -0700

    Fuck it, just use the euler angles.

--- public/js/drone.js
+++ public/js/drone.js
@@ -3,22 +3,25 @@ var Drone = function() {
   var material = new THREE.MeshBasicMaterial({
     color: 0xff0000,
   });
-  //this.add(new THREE.AxisHelper(100));
   var loader = new THREE.ColladaLoader();
   var self = this;
   loader.load('assets/ar-drone-2.dae', function(result) {
-    //result.scene.rotation = new THREE.Vector3(-Math.PI / 2, 0, -Math.PI / 2);
+    result.scene.rotation = new THREE.Vector3(-Math.PI / 2, 0, -Math.PI / 2);
     self.add(result.scene);
   });
 }
 Drone.prototype = Object.create(THREE.Object3D.prototype);
 
+var PI180 = Math.PI / 180.0;
+
 Drone.prototype.updateState = function(navdata) {
-  var r = navdata.demo.drone.camera.rotation;
-  var m = new THREE.Matrix4(r.m11, r.m12, r.m13, 0,
-                            r.m21, r.m22, r.m23, 0,
-                            r.m31, r.m32, r.m33, 0,
-                            0, 0, 0, 1)
+
+  var euler = navdata.demo.rotation;
+  var m = new THREE.Matrix4();
+  m.makeRotationFromEuler(new THREE.Vector3(euler.roll * PI180,
+                                            euler.yaw * -PI180,
+                                            euler.pitch * PI180),
+                          'YZX');
   this.matrix = m;
   this.matrixAutoUpdate = false;
   this.updateMatrixWorld(true);
