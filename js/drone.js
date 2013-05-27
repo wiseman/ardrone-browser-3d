e1e4d22325cd010fb0c90a1140f6441675c9614a
John Wiseman <jwiseman@morseproject.com>
2013-05-27 15:08:14 -0700

    Show "loading model..." status.

--- js/drone.js
+++ js/drone.js
@@ -1,23 +1,16 @@
-var Drone = function() {
+var Drone = function(model) {
   THREE.Object3D.call(this);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-  });
-  var loader = new THREE.ColladaLoader();
-  var self = this;
-  loader.load('assets/ar-drone-2.dae', function(result) {
-    result.scene.rotation = new THREE.Vector3(-Math.PI / 2, 0, -Math.PI / 2);
-    self.add(result.scene);
-  });
+  this.add(model);
 }
 Drone.prototype = Object.create(THREE.Object3D.prototype);
 
 var PI180 = Math.PI / 180.0;
 
 Drone.prototype.updateState = function(navdata) {
-
   var euler = navdata.demo.rotation;
   var m = new THREE.Matrix4();
+  // I couldn't find documentation on the AR.Drone Euler angle order,
+  // but this seems to work.
   m.makeRotationFromEuler(new THREE.Vector3(euler.roll * PI180,
                                             euler.yaw * -PI180,
                                             euler.pitch * PI180),
