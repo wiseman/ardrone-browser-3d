796847db2ae8bcf14e117a4beeb2a08b96a9020c
John Wiseman <jwiseman@morseproject.com>
2013-05-27 14:30:45 -0700

    Minor code cleanup.

--- public/js/drone.js
+++ public/js/drone.js
@@ -1,8 +1,5 @@
 var Drone = function() {
   THREE.Object3D.call(this);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-  });
   var loader = new THREE.ColladaLoader();
   var self = this;
   loader.load('assets/ar-drone-2.dae', function(result) {
@@ -15,9 +12,10 @@ Drone.prototype = Object.create(THREE.Object3D.prototype);
 var PI180 = Math.PI / 180.0;
 
 Drone.prototype.updateState = function(navdata) {
-
   var euler = navdata.demo.rotation;
   var m = new THREE.Matrix4();
+  // I couldn't find documentation on the AR.Drone Euler angle order,
+  // but this seems to work.
   m.makeRotationFromEuler(new THREE.Vector3(euler.roll * PI180,
                                             euler.yaw * -PI180,
                                             euler.pitch * PI180),
